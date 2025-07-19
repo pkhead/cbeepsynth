3a1aa30701fc7f857c87baddd4b2f3bce48a6140
pkhead <chromosoze@gmail.com>
2025-07-18 22:35:27 -0500

    fix wavetable oob bug

--- src/wavetables.c
+++ src/wavetables.c
@@ -2,15 +2,15 @@
 #include "util.h"
 #include "wavetables.h"
 
-float sine_wave_f[SINE_WAVE_LENGTH];
-double sine_wave_d[SINE_WAVE_LENGTH];
+float sine_wave_f[SINE_WAVE_LENGTH + 1];
+double sine_wave_d[SINE_WAVE_LENGTH + 1];
 static int need_init_wavetables = 1;
 
 void init_wavetables() {
     if (need_init_wavetables) {
         need_init_wavetables = 0;
 
-        for (int i = 0; i < SINE_WAVE_LENGTH; i++) {
+        for (int i = 0; i < SINE_WAVE_LENGTH + 1; i++) {
             sine_wave_d[i] = sin((double)i / SINE_WAVE_LENGTH * PI2);
             sine_wave_f[i] = sinf((float)i / SINE_WAVE_LENGTH * PI2f);
         }
--- src/wavetables.h
+++ src/wavetables.h
@@ -2,9 +2,12 @@
 #define _wavetables_h_
 
 // SINE_WAVE_LENGTH must be a power of 2!
+// add an extra element to the wavetable in case
+// synthesizer code uses the last index of the wavetable,
+// and then gets the next element for interpolation purposes.
 #define SINE_WAVE_LENGTH 256
-extern float sine_wave_f[SINE_WAVE_LENGTH];
-extern double sine_wave_d[SINE_WAVE_LENGTH];
-extern void init_wavetables();
+extern float sine_wave_f[SINE_WAVE_LENGTH + 1];
+extern double sine_wave_d[SINE_WAVE_LENGTH + 1];
+extern void init_wavetables(void);
 
 #endif

45bc9964c3233d167b24b678817247a97f7c3e98
pkhead <chromosoze@gmail.com>
2025-08-19 12:07:35 -0500

    fix bitcrusher effect

--- synth/src/fx/bitcrusher.c
+++ synth/src/fx/bitcrusher.c
@@ -9,7 +9,7 @@
 
 #define BITCRUSHER_OCTAVE_STEP 0.5
 #define BITCRUSHER_BASE_VOLUME 0.010
-
+#define ZERO_EPSILON 1e-16
 
 void bpbxsyn_effect_init_bitcrusher(bpbxsyn_context_s *ctx, bitcrusher_effect_s *inst) {
     *inst = (bitcrusher_effect_s){
@@ -36,10 +36,9 @@ void bitcrusher_tick(bpbxsyn_effect_s *p_inst, const bpbxsyn_tick_ctx_s *ctx) {
     double quantization_setting_end = inst->bit_crush[1];
 
     // const base_pitch: number = Config.keys[synth.song!.key].base_pitch; // TODO: What if there's a key change mid-song?
-    double base_pitch = ctx->base_pitch;
-    if (base_pitch == 0) {
+    int base_pitch = ctx->base_pitch;
+    if (base_pitch == 0)
         base_pitch = 12;
-    }
 
     const double freq_start = key_to_hz_d(base_pitch + 60) * pow(2.0, (BPBXSYN_BITCRUSHER_FREQ_MAX - freq_setting_start) * BITCRUSHER_OCTAVE_STEP);
     const double freq_end = key_to_hz_d(base_pitch + 60) * pow(2.0, (BPBXSYN_BITCRUSHER_FREQ_MAX - freq_setting_end) * BITCRUSHER_OCTAVE_STEP);
@@ -82,25 +81,32 @@ void bitcrusher_run(bpbxsyn_effect_s *p_inst, float **p_buffer,
     const double fold_level_scale = inst->fold_level_scale;
 
     for (size_t frame = 0; frame < frame_count; ++frame) {
-        float sample = (float)buffer[frame];
+        double sample = (double)buffer[frame];
+        
+        // it is necessary that this effect not compute when the input is
+        // silent, because of two reasons:
+        //  1. when silent, it generates a (very small) DC offset.
+        //  2. when silent, clicks occur when changing the quantization
+        //     parameter (probably because the DC offset changes).
+        if (fabs(sample) < ZERO_EPSILON) continue;
 
         phase += phase_delta;
         if (phase < 1.0) {
             prev_input = sample;
             sample = current_output;
         } else {
-            phase = fmodf(phase, 1.0);
+            phase = fmod(phase, 1.0);
             const double ratio = phase / phase_delta;
             
             const double lerped_input = sample + (prev_input - sample) * ratio;
             prev_input = sample;
             
             const double wrap_level = fold_level * 4.0;
-            const double wrapped_sample = fmod(((fmod((lerped_input + fold_level), wrap_level)) + wrap_level), wrap_level);
+            const double wrapped_sample = fmod((fmod(lerped_input + fold_level, wrap_level) + wrap_level), wrap_level);
             const double folded_sample = fold_level - fabs(fold_level * 2.0 - wrapped_sample);
             const double scaled_sample = folded_sample / scale;
             const double old_value = current_output;
-            const double new_value = ((int)(scaled_sample > 0 ? scaled_sample + 1 : scaled_sample)-.5) * scale;
+            const double new_value = (((int)(scaled_sample > 0 ? scaled_sample + 1 : scaled_sample))-.5) * scale;
             
             sample = old_value + (new_value - old_value) * ratio;
             current_output = new_value;
@@ -110,7 +116,7 @@ void bitcrusher_run(bpbxsyn_effect_s *p_inst, float **p_buffer,
         scale *= scale_scale;
         fold_level *= fold_level_scale;
 
-        buffer[frame] = (double)sample;
+        buffer[frame] = (float)sample;
     }
 
     if (fabs(prev_input) < FLUSH_ZERO_EPSILON) prev_input = 0.0;
